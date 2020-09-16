1a43bf82e7217010728d6d1b11b50df0030fa8e0
yogeshnile <yogeshnile786@gmail.com>
2020-09-16 14:08:52 +0530

    Blog project start

--- iCoder/asgi.py
+++ iCoder/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hello project.
+ASGI config for iCoder project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iCoder.settings')
 
 application = get_asgi_application()
--- iCoder/wsgi.py
+++ iCoder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hello project.
+WSGI config for iCoder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iCoder.settings')
 
 application = get_wsgi_application()
