b75277392934d2c19b927b1b4b8998d71c23b8a9
yogeshnile <yogeshnile786@gmail.com>
2020-09-16 19:46:29 +0530

    django message added

--- home/models.py
+++ home/models.py
@@ -9,3 +9,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=13)
     massage = models.TextField()
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
--- home/views.py
+++ home/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phonenumber']
+        msg = request.POST['massage']
+        
+        if len(name)<2 or len(email)<5 or len(phone)<10 or len(msg)<10:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, massage=msg)
+            contact.save()
+            messages.success(request, "Your message has been send")
+        
     return render(request, 'home/contact.html')
 
 def about(request):
