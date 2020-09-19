448efe2a96d20dd11edd2c59a10fbfcc1dea4e40
yogeshnile <yogeshnile786@gmail.com>
2020-09-19 13:44:13 +0530

    comment model develop

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/postcomment', views.Postcomment, name='Postcomment'),
     path('', views.bloghome, name='bloghome'),
     path('/<str:slug>', views.blogpost, name='blogpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, BlogComment
+from django.contrib import messages
 
 # Create your views here.
 def bloghome(request):
@@ -9,5 +10,22 @@ def bloghome(request):
 
 def blogpost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    context = {'post':post}
+    comment = BlogComment.objects.filter(post=post)
+    context = {'post':post, 'comment':comment}
     return render(request, 'blog/blogpost.html',context)
+
+def Postcomment(request):
+    if request.method == 'POST':
+        print("recevied")
+        comment = request.POST['comment']
+        user = request.user
+        postSno = request.POST['postSno']
+        post = Post.objects.get(sno=postSno)
+        # parent = request.POST.get('comment')
+
+        Scomment = BlogComment(comment=comment, user=user, post=post)
+        Scomment.save()
+        messages.success(request, "Comment Added Successfully")
+        return redirect(f"/blog/{post.slug}")
+        
+    return redirect('home')
