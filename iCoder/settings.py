110d0dfda4ad135e495122d65d827eb0852ddd1d
yogeshnile <yogeshnile786@gmail.com>
2020-09-19 17:09:58 +0530

    comment desing

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'blog.apps.BlogConfig',
+    'django.contrib.humanize', #custom datetime formate in template
 ]
 
 MIDDLEWARE = [
